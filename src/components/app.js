84fba85798be4aa9f757cfd0a6386d1cfb4697a7
Hector Coto Rojas <heavenhell64@gmail.com>
2016-11-06 16:23:20 -0600

    After learning how to configure and use ESlint, now it’s ready and settled for a good start.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import RedditData from './show_data';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+		<h2>Simple temp app to use thunk</h2>
+		<RedditData />
+      </div>
     );
   }
 }

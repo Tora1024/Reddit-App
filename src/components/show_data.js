cca15eba93a87052d0b6c0be5e02d8bc00c188a9
Hector Coto Rojas <heavenhell64@gmail.com>
2016-11-09 23:37:57 -0600

    Some changes on images

--- src/components/show_data.js
+++ src/components/show_data.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ReactImageFallback from 'react-image-fallback';
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = { imgError: false };
-		this.onImageError = this.onImageError.bind(this);
-		this.renderImage = this.renderImage.bind(this);
-	}
-
-	onImageError() {
-		this.setState({ imgError: true });
-	}
-
-	renderImage(post) {
-		if (!this.state.imgError) {
-			return <img alt={post.data.title} src={post.data.thumbnail} onError={this.onImageError} />;
-		}
-	}
-
 	renderPosts() {
 		if (Object.keys(this.props.posts).length === 0) {
 			return (
@@ -30,7 +13,11 @@ class PostsIndex extends Component {
 		console.log(this.props.posts.children);
 		return this.props.posts.children.map((post) => 
 			<li className="list-group-item" key={post.data.id}>
-				{ this.renderImage(post) }
+				<ReactImageFallback 
+					src={post.data.thumbnail}
+					initialImage="../../images/load.gif"
+					fallbackImage="../../images/load.gif"
+				/>
 				<span className="pull-xs-right">by: {post.data.author}</span>
 				<strong>{post.data.title}</strong>
 			</li>

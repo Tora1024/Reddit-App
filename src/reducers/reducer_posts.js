5a417e3922d247be55c672023c58a0bcfd891b7a
Hector Coto Rojas <heavenhell64@gmail.com>
2016-11-14 16:00:32 -0600

    Fixed a bug in which if I continue searching after an error, it kept showing just error instead of the new search

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,9 +5,9 @@ const INITIAL_STATE = { all: {}, error: '' };
 export default function (state = INITIAL_STATE, action) {
 	switch (action.type) {
 		case FETCH_POSTS:
-			return { ...state, all: action.payload.data };
+			return { ...state, all: action.payload.data, error: '' };
 		case ERROR:
-			return { ...state, error: action.payload };
+			return { ...state, error: action.payload.message };
 		default:
 			return state;
 	}
